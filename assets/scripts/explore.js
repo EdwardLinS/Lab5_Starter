20df4db48a391bec1507b47938b17d5f776c38b2
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-05-04 14:37:43 -0700

    testing branch: should be wrong

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,75 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+/**
+ * Loads all available voices and populates select element
+ *
+ * @param {Element} select
+ * @returns Array of voices
+ */
+const loadVoices = (select) => {
+  // stores all available voices
+  const voices = speechSynthesis.getVoices();
+
+  // populate select element
+  for (let i = 0; i < voices.length; i++) {
+    const voiceOption = document.createElement("option");
+    voiceOption.value = i; // index in voices array
+    voiceOption.textContent = voices[i].name;
+
+    select.appendChild(voiceOption);
+  }
+
+  return voices;
+};
+
+/**
+ * Speaks a message with a chosen voice
+ *
+ * @param {SpeechSynthesisVoice} chosenVoice
+ * @param {String} message
+ */
+const speakMessage = (chosenVoice, message) => {
+  // smiling face
+  const friend = document.querySelector("img[alt='Smiling face']");
+
+  // utterance object
+  const utterance = new SpeechSynthesisUtterance(message);
+  utterance.voice = chosenVoice;
+
+  // open mouth
+  utterance.addEventListener("start", () => {
+    friend.src = "assets/images/smiling-open.png";
+  });
+
+  // close mouth
+  utterance.addEventListener("end", () => {
+    friend.src = "assets/images/smiling.png";
+  });
+
+  speechSynthesis.speak(utterance);
+};
+
 function init() {
   // TODO
+  const voiceSelect = document.querySelector("select[name='voice']"); // select element
+
+  let voices = []; // stores all voices
+
+  // loads voices when loaded
+  speechSynthesis.onvoiceschanged = () => {
+    voices = loadVoices(voiceSelect);
+  };
+
+  // listen for button press
+  const playButton = document.querySelector("button");
+  const textToSpeak = document.getElementById("text-to-speak");
+
+  // speak if clicked
+  playButton.addEventListener("click", () => {
+    // speak if voice selected
+    if (voiceSelect.value !== "select") {
+      speakMessage(voices[voiceSelect.value], textToSpeak.value);
+    }
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,71 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+function updateVolumeIcon() {
+  const volumeIcon = document.querySelector("img[alt='Volume level 2']"); // volume icon level
+
+  // check volume level
+  if (volumeLevel === 0) {
+    volumeIcon.src = "assets/icons/volume-level-0.svg";
+  } else if (volumeLevel < 33) {
+    volumeIcon.src = "assets/icons/volume-level-1.svg";
+  } else if (volumeLevel < 67) {
+    volumeIcon.src = "assets/icons/volume-level-2.svg";
+  } else {
+    volumeIcon.src = "assets/icons/volume-level-3.svg";
+  }
+}
+
 function init() {
   // TODO
+  const hornSelect = document.getElementById("horn-select"); // horn input element
+  const hornImage = document.querySelector("img[alt='No image selected']"); // horn image
+  const hornAudio = document.querySelector("audio"); // horn audio
+
+  const volumeInput = document.getElementById("volume"); // volume input
+  const volumeIcon = document.querySelector("img[alt='Volume level 2']"); // volume icon level
+  let volumeLevel = volumeInput.value; // current volume level
+
+  const playButton = document.querySelector("button"); // play button
+
+  const jsConfetti = new JSConfetti();
+
+  // on horn selection change
+  hornSelect.addEventListener("change", (e) => {
+    hornImage.src = `assets/images/${e.target.value}.svg`;
+    hornAudio.src = `assets/audio/${e.target.value}.mp3`;
+  });
+
+  // on volume change
+  volumeInput.addEventListener("change", (e) => {
+    // update volume
+    volumeLevel = Number(e.target.value);
+
+    // check volume level
+    if (volumeLevel === 0) {
+      volumeIcon.src = "assets/icons/volume-level-0.svg";
+    } else if (volumeLevel < 33) {
+      volumeIcon.src = "assets/icons/volume-level-1.svg";
+    } else if (volumeLevel < 67) {
+      volumeIcon.src = "assets/icons/volume-level-2.svg";
+    } else {
+      volumeIcon.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+  // on button click
+  playButton.addEventListener("click", () => {
+    
+    // has horn selected
+    if (hornSelect.value !== "select") {
+
+      // confetti if party horn
+      if (hornSelect.value === "party-horn") {
+        jsConfetti.addConfetti();
+      }
+
+      hornAudio.volume = volumeLevel / 100;
+      hornAudio.play();
+    }
+  });
 }
